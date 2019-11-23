84d40c1a00dff4505e24c06f5684790950605acb
Josh Tracy <jjtadm@divvypay.com>
2019-11-23 12:24:01 -0700

    this works

--- board.py
+++ board.py
@@ -33,18 +33,85 @@ class TicTacToe():
                         try:
                             if self.board[row, col] == 'X' \
                                     and self.board[row, col + 1] == 'X' \
-                                    and self.board[row, col + 1] == 'X':
+                                    and self.board[row, col + 2] == 'X':
                                 return True
                         except IndexError:
                             pass
 
                         try:
-                            if self.board[row, col] == 'X' and self.board
+                            if self.board[row, col] == 'X' \
+                                    and self.board[row + 1, col + 1] == 'X' \
+                                    and self.board[row + 2, col + 2] == 'X':
+                                return True
+                        except IndexError:
+                            pass
 
+                        try:
+                            if self.board[row, col] == 'X' \
+                                    and self.board[row + 1, col - 1] == 'X' \
+                                    and self.board[row + 2, col - 2] == 'X':
+                                return True
+                        except IndexError:
+                            pass
+
+        if self.player[playerId] == 'O':
+            for row in range(n_rows):
+                for col in range(n_cols):
+                    if self.board[row, col] != '':
+                        try:
+                            if self.board[row, col] == 'O'\
+                                    and self.board[row + 1, col] == 'O'\
+                                    and self.board[row + 2, col] == 'O':
+                                return True
+                        except IndexError:
+                            pass
+
+                        try:
+                            if self.board[row, col] == 'O' \
+                                    and self.board[row, col + 1] == 'O' \
+                                    and self.board[row, col + 2] == 'O':
+                                return True
+                        except IndexError:
+                            pass
+
+                        try:
+                            if self.board[row, col] == 'O' \
+                                    and self.board[row + 1, col + 1] == 'O' \
+                                    and self.board[row + 2, col + 2] == 'O':
+                                return True
+                        except IndexError:
+                            pass
+
+                        try:
+                            if self.board[row, col] == 'O' \
+                                    and self.board[row + 1, col - 1] == 'O' \
+                                    and self.board[row + 2, col - 2] == 'O':
+                                return True
+                        except IndexError:
+                            pass
 
 
 tic_tac_toe = TicTacToe()
 tic_tac_toe.print_board()
+print('\n')
 tic_tac_toe.move(1, 1, 0)
 tic_tac_toe.print_board()
+print(f'Check winner: {tic_tac_toe.check_winner(0)}')
+print('\n')
 tic_tac_toe.move(2, 1, 1)
+tic_tac_toe.print_board()
+print(f'Check winner: {tic_tac_toe.check_winner(1)}')
+print('\n')
+tic_tac_toe.move(0, 0, 0)
+tic_tac_toe.print_board()
+print(f'Check winner: {tic_tac_toe.check_winner(0)}')
+print('\n')
+tic_tac_toe.move(2, 0, 1)
+tic_tac_toe.print_board()
+print(f'Check winner: {tic_tac_toe.check_winner(1)}')
+print('\n')
+tic_tac_toe.move(2, 2, 0)
+tic_tac_toe.print_board()
+print(f'Check winner: {tic_tac_toe.check_winner(0)}')
+print('\n')
+
