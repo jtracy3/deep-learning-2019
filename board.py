b97683326f94cfc0f82877eecff069e79158dfc0
kali <kaliwickens2010@gmail.com>
2019-12-08 17:18:55 -0700

    new check winner function with reward value

--- board.py
+++ board.py
@@ -101,5 +101,87 @@ class TicTacToe():
                     actions.append((row, col))
         return actions
 
+    def check_winner_reward(self):
+        """Checks if the game is over and return a possible winner.
+        There are 3 possible scenarios.
+            a) The game is over and we have a winner.
+            b) The game is over but it is a draw.
+            c) The game is not over.
+        Args:
+            current_player: An integer representing the current player.
+        Returns:
+            A bool representing the game over state.
+            An integer action value. (win: 1, loss: -1, draw: 0
+        """
+        n_rows, n_cols = self.current_board.shape
+
+        player_a = self.player
+        player_b = (self.player + 1) % 2
+
+        # Check for horizontal marks
+        for x in range(n_rows):
+            player_a_count = 0
+            player_b_count = 0
+            for y in range(n_cols):
+                if self.current_board[x][y] == self.player_map[player_a]:
+                    player_a_count += 1
+                elif self.current_board[x][y] == self.player_map[player_b]:
+                    player_b_count += 1
+            if player_a_count == n_cols:
+                return True, 1
+            elif player_b_count == n_cols:
+                return True, -1
+
+        # Check for vertical marks
+        for x in range(n_rows):
+            player_a_count = 0
+            player_b_count = 0
+            for y in range(n_cols):
+                if self.current_board[y][x] == self.player_map[player_a]:
+                    player_a_count += 1
+                elif self.current_board[y][x] == self.player_map[player_b]:
+                    player_b_count += 1
+            if player_a_count == n_rows:
+                return True, 1
+            elif player_b_count == n_rows:
+                return True, -1
+
+        # Check for major diagonal marks
+        player_a_count = 0
+        player_b_count = 0
+        for x in range(n_rows):
+            if self.current_board[x][x] == self.player_map[player_a]:
+                player_a_count += 1
+            elif self.current_board[x][x] == self.player_map[player_b]:
+                player_b_count += 1
+
+        if player_a_count == n_rows:
+            return True, 1
+        elif player_b_count == n_rows:
+            return True, -1
+
+        # Check for minor diagonal marks
+        player_a_count = 0
+        player_b_count = 0
+        for y in range(n_rows - 1, -1, -1):
+            x = 2 - y
+            if self.current_board[x][y] == self.player_map[player_a]:
+                player_a_count += 1
+            elif self.current_board[x][y] == self.player_map[player_b]:
+                player_b_count += 1
+
+        if player_a_count == n_rows:
+            return True, 1
+        elif player_b_count == n_rows:
+            return True, -1
+
+        # There are still moves left so the game is not over
+        actions = self.possible_actions()
+        if not actions:
+            return False, 0
+
+        # If there are no moves left the game is over without a winner
+        return True, 0
+
     def next_player(self):
         self.player = (self.player + 1) % 2
