c8c469f7b8493e84f93233386e57d6820a1fdf90
tjtiner <travisjtiner@gmail.com>
2019-12-08 22:19:57 -0700

    added model

--- board.py
+++ board.py
@@ -3,156 +3,199 @@ import numpy as np
 class TicTacToe():
 
     def __init__(self):
-        self.board = np.zeros((3, 3), dtype='str')
-        self.board[self.board == ''] = ' '
-        self.current_board = self.board
-        self.player = {0: 'X', 1: 'O'}
+        self.setup_board = np.zeros((3, 3), dtype='str')
+        self.setup_board[self.setup_board == ''] = ' '
+        self.current_board = self.setup_board
+        self.player = 0
+        self.player_map = {0: 'X', 1: 'O'}
 
     def print_board(self):
-        for line in self.board:
+        for line in self.current_board:
             print(line)
 
-    def move(self, row, column, playerId):
-        if self.board[row, column] != ' ':
+    def flatten_board(self):
+        flattened = [0] * 9
+        j = 0
+        for line in self.current_board:
+            for i in line:
+              if i == ' ':
+                flattened[j] = 0
+              elif i == 'X':
+                flattened[j] = 1
+              elif i == 'O':
+                flattened[j] = 2
+              j += 1
+        return flattened
+
+    def move(self, row, column):
+        if self.current_board[row, column] != ' ':
             raise Exception('Invalid placement')
-        self.board[row, column] = self.player[playerId]
+        self.current_board[row, column] = self.player_map[self.player]
 
-    def check_winner(self, playerId):
-        n_rows, n_cols = self.board.shape
-        if self.player[playerId] == 'X':
-            for row in range(n_rows):
-                for col in range(n_cols):
-                    if self.board[row, col] != ' ':
-                        try:
-                            if self.board[row, col] == 'X'\
-                                    and self.board[row + 1, col] == 'X'\
-                                    and self.board[row + 2, col] == 'X':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'X' \
-                                    and self.board[row, col + 1] == 'X' \
-                                    and self.board[row, col + 2] == 'X':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'X' \
-                                    and self.board[row + 1, col + 1] == 'X' \
-                                    and self.board[row + 2, col + 2] == 'X':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'X' \
-                                    and self.board[row + 1, col - 1] == 'X' \
-                                    and self.board[row + 2, col - 2] == 'X':
-                                return True
-                        except IndexError:
-                            pass
-
-        if self.player[playerId] == 'O':
-            for row in range(n_rows):
-                for col in range(n_cols):
-                    if self.board[row, col] != ' ':
-                        try:
-                            if self.board[row, col] == 'O'\
-                                    and self.board[row + 1, col] == 'O'\
-                                    and self.board[row + 2, col] == 'O':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'O' \
-                                    and self.board[row, col + 1] == 'O' \
-                                    and self.board[row, col + 2] == 'O':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'O' \
-                                    and self.board[row + 1, col + 1] == 'O' \
-                                    and self.board[row + 2, col + 2] == 'O':
-                                return True
-                        except IndexError:
-                            pass
-
-                        try:
-                            if self.board[row, col] == 'O' \
-                                    and self.board[row + 1, col - 1] == 'O' \
-                                    and self.board[row + 2, col - 2] == 'O':
-                                return True
-                        except IndexError:
-                            pass
+   # def check_winner(self):
+        # n_rows, n_cols = self.current_board.shape
+        # if self.player_map[self.player] == 'X':
+        #     for row in range(n_rows):
+        #         for col in range(n_cols):
+        #             if self.current_board[row, col] != ' ':
+        #                 try:
+        #                     if self.current_board[row, col] == 'X'\
+        #                             and self.current_board[row + 1, col] == 'X'\
+        #                             and self.current_board[row + 2, col] == 'X':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'X' \
+        #                             and self.current_board[row, col + 1] == 'X' \
+        #                             and self.current_board[row, col + 2] == 'X':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'X' \
+        #                             and self.current_board[row + 1, col + 1] == 'X' \
+        #                             and self.current_board[row + 2, col + 2] == 'X':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'X' \
+        #                             and self.current_board[row + 1, col - 1] == 'X' \
+        #                             and self.current_board[row + 2, col - 2] == 'X':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        # if self.player_map[self.player] == 'O':
+        #     for row in range(n_rows):
+        #         for col in range(n_cols):
+        #             if self.current_board[row, col] != ' ':
+        #                 try:
+        #                     if self.current_board[row, col] == 'O'\
+        #                             and self.current_board[row + 1, col] == 'O'\
+        #                             and self.current_board[row + 2, col] == 'O':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'O' \
+        #                             and self.current_board[row, col + 1] == 'O' \
+        #                             and self.current_board[row, col + 2] == 'O':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'O' \
+        #                             and self.current_board[row + 1, col + 1] == 'O' \
+        #                             and self.current_board[row + 2, col + 2] == 'O':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
+        #
+        #                 try:
+        #                     if self.current_board[row, col] == 'O' \
+        #                             and self.current_board[row + 1, col - 1] == 'O' \
+        #                             and self.current_board[row + 2, col - 2] == 'O':
+        #                         return True
+        #                 except IndexError:
+        #                     pass
 
     def possible_actions(self):
-        n_rows, n_cols = self.board.shape
+        n_rows, n_cols = self.current_board.shape
         actions = []
         for row in range(n_rows):
             for col in range(n_cols):
-                if self.board[row, col] == ' ':
+                if self.current_board[row, col] == ' ':
                     actions.append((row, col))
         return actions
 
+    def check_winner(self):
+        """Checks if the game is over and return a possible winner.
+        There are 3 possible scenarios
+            a) The game is over and we have a winner.
+            b) The game is over but it is a draw.
+            c) The game is not over.
+        Args:
+            Takes a TicTacToe board
+        Returns:
+            A bool representing the game over state.
+            An integer action value. (win: 1, loss: -1, draw: 0)
+        """
+        n_rows, n_cols = self.current_board.shape
 
-    def play_game(self, type='random'):
-        turn = 0
-        play = True
-        if type == 'random':
-            random = np.random
-        actions = [(0, 0), (0, 1), (0, 2),
-                   (1, 0), (1, 1), (1, 2),
-                   (2, 0), (2, 1), (2, 2)]
-        while play:
-            m = len(actions)
-            playerId = turn % 2
-            pick = random.randint(0, m)
-            move_ind = actions.pop(pick)
-            self.move(*move_ind, playerId)
-            self.print_board()
-            if self.check_winner(playerId):
-                play = False
-                print(f'Player {playerId} - ({self.player[playerId]}) is the winner!')
-            turn += 1
+        player_a = self.player
+        player_b = (self.player + 1) % 2
 
+        # Check for horizontal marks
+        for x in range(n_rows):
+            player_a_count = 0
+            player_b_count = 0
+            for y in range(n_cols):
+                if self.current_board[x][y] == self.player_map[player_a]:
+                    player_a_count += 1
+                elif self.current_board[x][y] == self.player_map[player_b]:
+                    player_b_count += 1
+            if player_a_count == n_cols:
+                return True, 1
+            elif player_b_count == n_cols:
+                return True, -1
 
+        # Check for vertical marks
+        for x in range(n_rows):
+            player_a_count = 0
+            player_b_count = 0
+            for y in range(n_cols):
+                if self.current_board[y][x] == self.player_map[player_a]:
+                    player_a_count += 1
+                elif self.current_board[y][x] == self.player_map[player_b]:
+                    player_b_count += 1
+            if player_a_count == n_rows:
+                return True, 1
+            elif player_b_count == n_rows:
+                return True, -1
 
+        # Check for major diagonal marks
+        player_a_count = 0
+        player_b_count = 0
+        for x in range(n_rows):
+            if self.current_board[x][x] == self.player_map[player_a]:
+                player_a_count += 1
+            elif self.current_board[x][x] == self.player_map[player_b]:
+                player_b_count += 1
 
-tic_tac_toe = TicTacToe()
-tic_tac_toe.print_board()
-print('\n')
+        if player_a_count == n_rows:
+            return True, 1
+        elif player_b_count == n_rows:
+            return True, -1
 
-tic_tac_toe.play_game()
+        # Check for minor diagonal marks
+        player_a_count = 0
+        player_b_count = 0
+        for y in range(n_rows - 1, -1, -1):
+            x = 2 - y
+            if self.current_board[x][y] == self.player_map[player_a]:
+                player_a_count += 1
+            elif self.current_board[x][y] == self.player_map[player_b]:
+                player_b_count += 1
 
-# tic_tac_toe.move(1, 1, 0)
-# tic_tac_toe.print_board()
-# print(f'Possible moves: {tic_tac_toe.possible_actions()}')
-# print(f'Check winner: {tic_tac_toe.check_winner(0)}')
-# print('\n')
-# tic_tac_toe.move(2, 1, 1)
-# tic_tac_toe.print_board()
-# print(f'Possible moves: {tic_tac_toe.possible_actions()}')
-# print(f'Check winner: {tic_tac_toe.check_winner(1)}')
-# print('\n')
-# tic_tac_toe.move(0, 0, 0)
-# tic_tac_toe.print_board()
-# print(f'Possible moves: {tic_tac_toe.possible_actions()}')
-# print(f'Check winner: {tic_tac_toe.check_winner(0)}')
-# print('\n')
-# tic_tac_toe.move(2, 0, 1)
-# tic_tac_toe.print_board()
-# print(f'Possible moves: {tic_tac_toe.possible_actions()}')
-# print(f'Check winner: {tic_tac_toe.check_winner(1)}')
-# print('\n')
-# tic_tac_toe.move(2, 2, 0)
-# tic_tac_toe.print_board()
-# print(f'Possible moves: {tic_tac_toe.possible_actions()}')
-# print(f'Check winner: {tic_tac_toe.check_winner(0)}')
-# print('\n')
+        if player_a_count == n_rows:
+            return True, 1
+        elif player_b_count == n_rows:
+            return True, -1
 
+        # There are still moves left so the game is not over
+        actions = self.possible_actions()
+        if actions:
+            return False, 0
+
+        # If there are no moves left the game is over without a winner
+        return True, 0
+
+    def next_player(self):
+        self.player = (self.player + 1) % 2
---
+++ play.py
@@ -0,0 +1,154 @@
+from board import TicTacToe
+import numpy as np
+from copy import deepcopy
+import pickle
+import random
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+model = pickle.load(open('dnn_model.pkl', 'rb'))
+# def play_game():
+#     game_data = dict(moves=list(), board_history=list(), winner=None)
+#     board = TicTacToe()
+#     play = True
+#     while play and board.possible_actions() != []:
+#         actions = board.possible_actions()
+#         print(actions)
+#         m = len(actions)
+#         pick = np.random.randint(0, m)
+#         move_ind = actions[pick]
+#         board.move(*move_ind)
+#         if board.check_winner():
+#             play = False
+#             game_data['winner'] = board.player
+#         elif not board.check_winner() and board.possible_actions() == []:
+#             play = False
+#             game_data['winner'] = 0.5
+#         game_data['moves'].append((board.player, move_ind))
+#         game_data['board_history'].append(deepcopy(board.current_board))
+#         board.next_player()
+#     return game_data
+
+def bestMove(board, model, player, rnd=0):
+    scores = []
+    moves = board.possible_actions()
+
+    # Make predictions for each possible move
+    for i in range(len(moves)):
+        future = deepcopy(board)
+        future.move(*moves[i])
+
+        flattened_board = np.array(future.flatten_board())
+        flattened_board = flattened_board.reshape((-1,9))
+        prediction = model.predict(flattened_board)[0]
+        if player == 0:
+            winPrediction = prediction[1]
+            lossPrediction = prediction[2]
+        else:
+            winPrediction = prediction[2]
+            lossPrediction = prediction[1]
+        drawPrediction = prediction[0]
+
+        if winPrediction - lossPrediction > 0:
+            scores.append(winPrediction - lossPrediction)
+        else:
+            scores.append(drawPrediction - lossPrediction)
+
+    # Choose the best move with a random factor
+    bestMoves = np.flip(np.argsort(scores))
+    for i in range(len(bestMoves)):
+        if random.random() * rnd < 0.5:
+            return moves[bestMoves[i]]
+
+    # Choose a move completely at random
+    return moves[random.randint(0, len(moves) - 1)]
+
+def play_game(p1=None,p2=None,rnd=0):
+    game_data = dict(moves=list(), board_history=list(), winner=None)
+    board = TicTacToe()
+    play = True
+    actions = board.possible_actions()
+    while play and actions:
+        actions = board.possible_actions()
+        #print(actions)
+        m = len(actions)
+        if board.player == 0 and p1 != None:
+          move_ind = bestMove(board, p1, board.player, rnd)
+          board.move(*move_ind)
+        elif board.player == 1 and p2 != None:
+          move_ind = bestMove(board, p2, board.player, rnd)
+          board.move(*move_ind)
+        else:
+          pick = np.random.randint(0, m)
+          move_ind = actions[pick]
+          board.move(*move_ind)
+
+        a_winner, reward = board.check_winner()
+        if a_winner and reward != 0:
+            play = False
+            game_data['winner'] = board.player
+        elif a_winner and reward == 0:
+            play = False
+            game_data['winner'] = 0.5
+        game_data['moves'].append((board.player, move_ind))
+        game_data['board_history'].append(deepcopy(board.current_board))
+        board.next_player()
+    return game_data
+
+
+# game_data = play_game_reward()
+# print(game_data)
+
+def gather_game_results(n_games, p1=None, p2=None, rnd=0):
+    results = dict(x_wins=0, o_wins=0, draws=0)
+    for i in range(n_games):
+        sim_game = play_game(p1=p1,p2=p2,rnd=rnd)
+        #sim_game = play_game_reward()
+        if sim_game['winner'] == 0:
+            results['x_wins'] += 1
+        elif sim_game['winner'] == 1:
+            results['o_wins'] += 1
+        else:
+            results['draws'] += 1
+    x_win_pct = results['x_wins'] / n_games
+    o_win_pct = results['o_wins'] / n_games
+    draw_pct = results['draws'] / n_games
+
+    print(f'The winning percentage for X was {x_win_pct*100:.2f}% in {n_games} random simulations')
+    print(f'The winning percentage for O was {o_win_pct*100:.2f}% in {n_games} random simulations')
+    print(f'The percentage of draws was {draw_pct*100:.2f}% in {n_games} random simulations')
+
+game_data = play_game(p1=model)
+print(game_data)
+
+gather_game_results(100,p1=model)
+# gather_game_results(1000)
+
+# def simulate_games(n_iter):
+#     results = dict(data=[], value=[], n_moves=[])
+#     for i in range(n_iter):
+#         sim_game = play_game()
+#         results['data'].append(sim_game[0])
+#         results['value'].append(sim_game[1])
+#         results['n_moves'].append(sim_game[2])
+#     return results
+
+# def create_mock_data(results, n_states, seed=0):
+#     tensors = list()
+#     values = list()
+#     np.random.seed(seed)
+#     for i in n_states:
+#         random_game_number = np.random.randint(n_states)
+#         random_game = results['data'][random_game_number]
+#         n_states_from_random_game = results['n_moves'][random_game_number]
+#         game_value = results['value'][random_game_number]
+#         random_state_number = np.random.randint(n_states_from_random_game)
+#         random_state = random_game[random_state_number][0]
+#         random_state_player = random_game[random_state_number][1]
+#         encoded_board = encode.encode_board(random_state, random_state_player)
+#         tensors.append(encoded_board)
+#         values.append(game_value)
+#
+#     return tensors, values
